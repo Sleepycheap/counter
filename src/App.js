f671ca5117bc082ed6bf06de468393fb54b26496
Sleepycheap <anthony.vaut@gmail.com>
2026-06-03 17:08:14 -0500

    backup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import ReactDOM from "react-dom/client";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
+
+  function handleStepUp() {}
+
+  return (
+    <div>
+      <span className="step-div">
+        <button className="btn">&minus;</button>
+        <p>Step: {step}</p>
+        <button className="btn">&#43;</button>
+      </span>
+
+      <span className="step-div">
+        <button className="btn">&minus;</button>
+        <p>Count: {count}</p>
+        <button className="btn">&#43;</button>
+      </span>
+
+      <span>Today is {"date"}</span>
+    </div>
+  );
+}
