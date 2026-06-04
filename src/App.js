e713134b8f01a339c0e84cb817197e2cf2a0a339
Sleepycheap <anthony.vaut@gmail.com>
2026-06-04 15:47:03 -0500

    backup

--- src/App.js
+++ src/App.js
@@ -10,26 +10,67 @@ export default function App() {
 }
 
 function Counter() {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
-  function handleStepUp() {}
+  function handleClick() {
+    setStep(1);
+    setCount(0);
+  }
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
 
   return (
     <div>
-      <span className="step-div">
-        <button className="btn">&minus;</button>
-        <p>Step: {step}</p>
-        <button className="btn">&#43;</button>
-      </span>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        {/* 
+        <button className="btn" onClick={() => setStep((s) => s - 1)}>
+          &minus;
+        </button> */}
+        <span>Step: {step}</span>
+        {/* <button className="btn" onClick={() => setStep((s) => s + 1)}>
+          &#43;
+        </button> */}
+      </div>
 
-      <span className="step-div">
-        <button className="btn">&minus;</button>
-        <p>Count: {count}</p>
-        <button className="btn">&#43;</button>
-      </span>
+      <div>
+        <button className="btn" onClick={() => setCount((c) => c - step)}>
+          &minus;
+        </button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button className="btn" onClick={() => setCount((c) => c + step)}>
+          &#43;
+        </button>
+      </div>
 
-      <span>Today is {"date"}</span>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+              ? `${count} days from now is `
+              : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleClick}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
